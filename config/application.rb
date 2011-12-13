501fdf480cc815df996c155e0c81024bbb51b1bc
Anton Shishkin <a.shishkin.b@gmail.com>
2011-12-13 14:08:58 +0400

    удалил странные тесты и поправил скаффолд

--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,11 @@ module Antientropy
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    config.generators do |g|
+      g.test_framework :rspec, :view_specs => false, :helper_specs => false, 
+                             :routing_specs => false, :request_specs => false
+      g.fixture_replacement :factory_girl, :dir => "spec/factories"
+    end
   end
 end
