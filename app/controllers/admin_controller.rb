dae9aae3683ffad5c77ab11471a9318afd55e08c
Anton Shishkin <a.shishkin.b@gmail.com>
2011-12-15 18:10:22 +0400

    starting

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,13 @@
 class AdminController < ApplicationController
+  before_filter :authenticate
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "odmin" && password == "trololo"
+    end
+  end
+
   def index
+    @quests = Quest.all
   end
 end
