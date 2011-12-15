b8a5ed96ff6dcfdca7a19c6c06a30a13e74fdb88
Stepan Kirushkin <stepan@leadsleader.ru>
2011-12-15 18:43:57 +0400

    запилил контролы

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,23 +8,72 @@
 //= require jquery_ujs
 //= require_tree .
 
-//Animate content
-  $(document).ready(function() {
-    $("[nav-item-id='1']").click(function() {
-      $("#nav-page-holder").animate({
-        marginLeft: -0
-      });
+// animate content
+$(document).ready(function() {
+  var w = screen.width-150;
+  var c = $('.nav-page').size();
 
-      $("[nav-item-id='1']").addClass("active");
-      $("[nav-item-id='2']").removeClass("active");
-    });
+  $('.nav-page').css("width", w)
 
-    $("[nav-item-id='2']").click(function() {
-      $("#nav-page-holder").animate({
-        marginLeft: -1600
-      });
+  $('.ctrl-prev').click(function() {
+    var p = parseInt($('#nav-page-holder').css('margin-left'));
+    if ((p < 0) && ((p / w) - Math.floor(p / w) == 0))
+      $('[nav-item-id=' + Math.abs(p / w) + ']').click()
+  })
 
-      $("[nav-item-id='2']").addClass("active");
-      $("[nav-item-id='1']").removeClass("active");
-    });
-  });
+  $('.ctrl-next').click(function() {
+    var p = parseInt($('#nav-page-holder').css('margin-left'));
+    if ((p > -w * (c-1)) && ((p / w) - Math.floor(p / w) == 0))
+      $('[nav-item-id=' + (Math.abs(p / w)+2) + ']').click()
+  })
+    
+  $('[nav-item-id=1]').click(function() {
+    $('#nav-page-holder').animate({ marginLeft: 0 })
+
+    $('[nav-item-id=1]').addClass('active')
+    $('[nav-item-id=2]').removeClass('active')
+    $('[nav-item-id=3]').removeClass('active')
+    $('[nav-item-id=4]').removeClass('active')
+    $('[nav-item-id=5]').removeClass('active')
+  })
+
+  $('[nav-item-id=2]').click(function() {
+    $('#nav-page-holder').animate({ marginLeft: -w * 1 })
+
+    $('[nav-item-id=2]').addClass('active')
+    $('[nav-item-id=1]').removeClass('active')
+    $('[nav-item-id=3]').removeClass('active')
+    $('[nav-item-id=4]').removeClass('active')
+    $('[nav-item-id=5]').removeClass('active')
+  })
+
+  $('[nav-item-id=3]').click(function() {
+    $('#nav-page-holder').animate({ marginLeft: -w * 2 })
+
+    $('[nav-item-id=3]').addClass('active')
+    $('[nav-item-id=1]').removeClass('active')
+    $('[nav-item-id=2]').removeClass('active')
+    $('[nav-item-id=4]').removeClass('active')
+    $('[nav-item-id=5]').removeClass('active')
+  })
+
+  $('[nav-item-id=4]').click(function() {
+    $('#nav-page-holder').animate({ marginLeft: -w * 3 })
+
+    $('[nav-item-id=4]').addClass('active')
+    $('[nav-item-id=1]').removeClass('active')
+    $('[nav-item-id=2]').removeClass('active')
+    $('[nav-item-id=3]').removeClass('active')
+    $('[nav-item-id=5]').removeClass('active')
+  })
+
+  $('[nav-item-id=5]').click(function() {
+    $('#nav-page-holder').animate({ marginLeft: -w * 4 })
+
+    $('[nav-item-id=5]').addClass('active')
+    $('[nav-item-id=1]').removeClass('active')
+    $('[nav-item-id=2]').removeClass('active')
+    $('[nav-item-id=3]').removeClass('active')
+    $('[nav-item-id=4]').removeClass('active')
+  })
+})
