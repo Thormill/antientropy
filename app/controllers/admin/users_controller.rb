6819f75016bd17bf55fe321e66c605ed5876a6c2
Anton Shishkin <a.shishkin.b@gmail.com>
2011-12-19 14:36:48 +0400

    users routes, controller + show view

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,6 +2,16 @@ class Admin::UsersController < ApplicationController
   respond_to :js
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:quest])
+    if(@user.save)
+            redirect_to admin_users_path, :notice => 'Yeah!'
+    else
+            redirect_to new_admin_user_path(@user), :notice => 'mistake'
+    end
   end
 
   def list
@@ -9,10 +19,18 @@ class Admin::UsersController < ApplicationController
   end
 
   def activate
+    @user = user.find(params[:id])
+    @user.activate!
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def delete
+    @user = User.find(params[:id])
+    @user.destroy
+    #redirect_to(admin_users_url)
   end
 
 end
