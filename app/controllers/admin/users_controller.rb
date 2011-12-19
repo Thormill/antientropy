5f96a09392b86d4f4cfd843e1ae75d4ee7b67372
Anton Shishkin <a.shishkin.b@gmail.com>
2011-12-19 15:37:56 +0400

    админка для юзеров без css

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,10 @@
 class Admin::UsersController < ApplicationController
   respond_to :js
 
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
@@ -8,14 +12,24 @@ class Admin::UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if(@user.save)
-            redirect_to admin_users_path
+      @user.activate!
+      redirect_to admin_users_path
     else
-            redirect_to new_admin_user_path(@user)
+      redirect_to new_admin_user_path(@user)
     end
   end
 
-  def index
-    @users = User.all
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      redirect_to admin_users_path
+    else
+      render :action => "edit"
+    end
   end
 
   def activate
