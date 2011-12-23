450814ef7572a9b91958c710d6e683a0a1a6026a
Stepan Kirushkin <stepan@leadsleader.ru>
2011-12-23 19:18:36 +0400

    правки главное страницы

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,8 @@ class SessionsController < ApplicationController
   before_filter :require_login, :only => [:destroy]
 
   def create
-    @notice = login(params[:email], params[:password], params[:remember]) ? I18n.t('user.login.done') : I18n.t('user.login.error')
-
-    unless logged_in?
+    login(params[:email], params[:password], params[:remember])
+    if logged_in?
       redirect_to :root
     else
       redirect_to :root
